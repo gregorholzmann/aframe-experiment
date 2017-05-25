80d84c0e7dd52034bb333fdfd6d8124698ca54ba
Gregor Holzmann <debrisplease@gmail.com>
2017-05-25 11:22:03 -0700

    configure store and fallback to local JSON file due to API issues

--- components/frame-generator.js
+++ components/frame-generator.js
@@ -1,93 +1,70 @@
 import { getData } from '../services/data-fetch';
 import { bootstrap } from '../actions/';
+import { store } from '../index.js';
 
 
 AFRAME.registerComponent('frame-generator', {
-  tick: function () {
+  update: function () {
     // Solution for Creating Entities.
     let sceneEl = document.querySelector('a-scene');
+    let people = store.getState();
 
-    // console.log(sceneEl.systems.redux.store.getData());
+    people.forEach(function(person, i) {
+        // create frame for each person
+        let frameEl = document.createElement('a-entity');
+        frameEl.setAttribute('obj-model', { 'obj': '#frame-obj', 'mtl': '#frame-mtl'});
+        frameEl.setAttribute('position', {x: -1.5, y: 1, z: -(10 * i) - .5});
+        frameEl.setAttribute('rotation', {x: 0, y: 90, z: 0});
+        frameEl.setAttribute('scale', {x: .05, y: .05, z: .05});
+        frameEl.setAttribute('frame-hover', true);
 
-    // function select(state) {
-    //   return state
-    // }
+        // add image as child of frame
+        let imgEl = document.createElement('a-image');
+        imgEl.setAttribute('src', person.acf.user_image.sizes.large);
+        imgEl.setAttribute('position', {x: -.5, y: 0, z: 0});
+        imgEl.setAttribute('rotation', {x: -90, y: 0, z: 0});
+        imgEl.setAttribute('height', 1.75);
 
-    // let currentValue
-    // function handleChange() {
-    //   let previousValue = currentValue
-    //   currentValue = select(store.getState())
+        // add name as child of frame
+        let nameEl = document.createElement('a-text');
+        nameEl.setAttribute('value', person.name);
+        nameEl.setAttribute('position', {x: -0.5, y: 0.15, z: 0.8});
+        nameEl.setAttribute('rotation', {x: -90, y: 0, z: 0});
+        nameEl.setAttribute('align', 'center');
+        nameEl.setAttribute('width', '5');
 
-    //   if (previousValue !== currentValue) {
-    //     console.log('Some deep nested property changed from', previousValue, 'to', currentValue)
-    //   }
-    // }
+        // add title as child of frame
+        let titleEl = document.createElement('a-text');
+        titleEl.setAttribute('value', person.acf.job_title);
+        titleEl.setAttribute('position', {x: -0.5, y: 0.15, z: 1});
+        titleEl.setAttribute('rotation', {x: -90, y: 0, z: 0});
+        titleEl.setAttribute('align', 'center');
+        titleEl.setAttribute('width', '3');
 
-    // let unsubscribe = store.subscribe(handleChange);
-    // unsubscribe();
-    // console.log(store.getState());
+        // add color overlay as child of frame
+        let colorPlateEl = document.createElement('a-entity');
+        colorPlateEl.setAttribute('geometry', 'primitive: plane');
+        colorPlateEl.setAttribute('material', {'transparent': true, 'opacity': .25, 'color': person.acf.favorite_color});
+        colorPlateEl.setAttribute('position', {x: -0.5, y: 0, z: 0});
+        colorPlateEl.setAttribute('rotation', {x: -90, y: 0, z: 0});
+        colorPlateEl.setAttribute('scale', {x: 1, y: 1.75, z: 1});
+        colorPlateEl.setAttribute('class', 'colorplate');
+        colorPlateEl.setAttribute('visible', false);
 
+        let colorPlateTextEl = document.createElement('a-text');
+        colorPlateTextEl.setAttribute('value', 'Favorite color: ' + person.acf.favorite_color);
+        colorPlateTextEl.setAttribute('position', {x: -0.5, y: 0, z: 0});
+        colorPlateTextEl.setAttribute('rotation', {x: 0, y: 0, z: 0});
+        colorPlateTextEl.setAttribute('width', '2');
 
-    // getData().then(function(data) {
-    //   let people = data;
+        colorPlateEl.appendChild(colorPlateTextEl);
 
-    //   people.forEach(function(person, i) {
-    //     // create frame for each person
-    //     let frameEl = document.createElement('a-entity');
-    //     frameEl.setAttribute('obj-model', { 'obj': '#frame-obj', 'mtl': '#frame-mtl'});
-    //     frameEl.setAttribute('position', {x: -1.5, y: 1, z: -(10 * i) - .5});
-    //     frameEl.setAttribute('rotation', {x: 0, y: 90, z: 0});
-    //     frameEl.setAttribute('scale', {x: .05, y: .05, z: .05});
-    //     frameEl.setAttribute('frame-hover', true);
+        frameEl.appendChild(nameEl);
+        frameEl.appendChild(titleEl);
+        frameEl.appendChild(imgEl);
+        frameEl.appendChild(colorPlateEl);
 
-    //     // // add image as child of frame
-    //     // let imgEl = document.createElement('a-image');
-    //     // imgEl.setAttribute('src', person.acf.user_image.sizes.large);
-    //     // imgEl.setAttribute('position', {x: -.5, y: 0, z: 0});
-    //     // imgEl.setAttribute('rotation', {x: -90, y: 0, z: 0});
-    //     // imgEl.setAttribute('height', 1.75);
-
-    //     // // add name as child of frame
-    //     // let nameEl = document.createElement('a-text');
-    //     // nameEl.setAttribute('value', person.name);
-    //     // nameEl.setAttribute('position', {x: -0.5, y: 0.15, z: 0.8});
-    //     // nameEl.setAttribute('rotation', {x: -90, y: 0, z: 0});
-    //     // nameEl.setAttribute('align', 'center');
-    //     // nameEl.setAttribute('width', '5');
-
-    //     // // add title as child of frame
-    //     // let titleEl = document.createElement('a-text');
-    //     // titleEl.setAttribute('value', person.acf.job_title);
-    //     // titleEl.setAttribute('position', {x: -0.5, y: 0.15, z: 1});
-    //     // titleEl.setAttribute('rotation', {x: -90, y: 0, z: 0});
-    //     // titleEl.setAttribute('align', 'center');
-    //     // titleEl.setAttribute('width', '3');
-
-    //     // // add color overlay as child of frame
-    //     // let colorPlateEl = document.createElement('a-entity');
-    //     // colorPlateEl.setAttribute('geometry', 'primitive: plane');
-    //     // colorPlateEl.setAttribute('material', {'transparent': true, 'opacity': .25, 'color': person.acf.favorite_color});
-    //     // colorPlateEl.setAttribute('position', {x: -0.5, y: 0, z: 0});
-    //     // colorPlateEl.setAttribute('rotation', {x: -90, y: 0, z: 0});
-    //     // colorPlateEl.setAttribute('scale', {x: 1, y: 1.75, z: 1});
-    //     // colorPlateEl.setAttribute('class', 'colorplate');
-    //     // colorPlateEl.setAttribute('visible', false);
-
-    //     // let colorPlateTextEl = document.createElement('a-text');
-    //     // colorPlateTextEl.setAttribute('value', 'Favorite color: ' + person.acf.favorite_color);
-    //     // colorPlateTextEl.setAttribute('position', {x: -0.5, y: 0, z: 0});
-    //     // colorPlateTextEl.setAttribute('rotation', {x: 0, y: 0, z: 0});
-    //     // colorPlateTextEl.setAttribute('width', '2');
-
-    //     // colorPlateEl.appendChild(colorPlateTextEl);
-
-    //     // frameEl.appendChild(nameEl);
-    //     // frameEl.appendChild(titleEl);
-    //     // frameEl.appendChild(imgEl);
-    //     // frameEl.appendChild(colorPlateEl);
-
-    //     sceneEl.appendChild(frameEl);                
-    //   });
-    // })
+        sceneEl.appendChild(frameEl);
+      });
   }
 });
--- index.js
+++ index.js
@@ -7,15 +7,18 @@ if (module.hot) {
 import { hoverHandler } from './components/hover-handler.js';
 import { frameHover } from './components/frame-hover.js';
 import { frameGenerator } from './components/frame-generator.js';
-import { reducer } from './components/reducer.js';
+
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import * as reducers from './reducers';
 import * as actions from './actions';
 import thunk from 'redux-thunk'
 
-const store = createStore(
-  reducers.employees,
-  applyMiddleware(thunk)
+export const store = createStore(
+    reducers.employees,
+    composeWithDevTools(
+        applyMiddleware(thunk),
+    )
 )
 
 store.dispatch(actions.bootstrap());
